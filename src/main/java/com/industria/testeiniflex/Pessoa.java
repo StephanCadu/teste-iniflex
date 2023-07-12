5f292e7bc05fa3b63a1c63a13c4266565dfa38a9
Cadu <cdauk@hotmail.com>
2023-07-12 17:26:21 -0300

    feat: cria classe Pessoa

---
+++ src/main/java/com/industria/testeiniflex/Pessoa.java
@@ -0,0 +1,38 @@
+package com.industria.testeiniflex;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+  private String nome;
+
+  private LocalDate dataNascimento;
+
+  /**
+   * Construtor com argumentos.
+   * 
+   * @param nome : Nome da pessoa.
+   * @param dataNascimento : Data de nascimento da pessoa.
+   */
+  public Pessoa(String nome, LocalDate dataNascimento) {
+    this.nome = nome;
+    this.dataNascimento = dataNascimento;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public LocalDate getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(LocalDate dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+}
