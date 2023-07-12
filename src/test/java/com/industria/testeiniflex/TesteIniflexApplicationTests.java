f573aa579c71e932454fb8d58dcbabd9e64250b8
Cadu <cdauk@hotmail.com>
2023-07-12 17:21:37 -0300

    commit inicial

---
+++ src/test/java/com/industria/testeiniflex/TesteIniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industria.testeiniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteIniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
